c3e0f87131084c26bc84c22f8329c5cd9a032cdf
LEIDY JURANI ROMERO SANTANA <lj.romero@sis.virtual.uniandes.edu.co>
2018-09-29 12:49:56 -0500

    juntas

--- superAndes-jdo/src/main/java/uniandes/isis2304/superAndes/negocio/VOTipoProducto.java
+++ superAndes-jdo/src/main/java/uniandes/isis2304/superAndes/negocio/VOTipoProducto.java
@@ -1,5 +1,10 @@
 package uniandes.isis2304.superAndes.negocio;
-
+/**
+ * 
+ * @author lj.romero
+ *
+ */
 public interface VOTipoProducto {
-
+	public String getNombre();
+	public String toString();
 }
