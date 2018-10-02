1afe226aa8f02ca041783c6dfa1a53e8f8f38125
Majo Ocampo <mj.ocampov@uniandes.edu.co>
2018-10-02 15:29:44 -0500

    Pocos cambios

--- superAndes-jdo/src/main/java/uniandes/isis2304/superAndes/interfazApp/DialogoRegistrarEmpresa.java
+++ superAndes-jdo/src/main/java/uniandes/isis2304/superAndes/interfazApp/DialogoRegistrarEmpresa.java
@@ -73,9 +73,6 @@ public class DialogoRegistrarEmpresa extends JDialog implements ActionListener{
 	 */
 	private JButton btnCancelar;
 
-
-<<<<<<< HEAD
-
 	// --------------------------------------------------------
 	// Constructores
 	// --------------------------------------------------------
@@ -97,14 +94,9 @@ public class DialogoRegistrarEmpresa extends JDialog implements ActionListener{
 
 		add( lbNombre );
 		add( txtNombre );
-=======
-        setLayout( new GridLayout( 7, 2 ) );
-        this.setSize( 600, 200 );
-        setTitle( "Agregar empresa" );
 
         lbNit = new JLabel( "NIT:" );
         txtNit = new JTextField( );
->>>>>>> d36ad856604d5bf05b4ade53b7bafb81df620c6f
 
 		lbCorreo = new JLabel( "Correo:" );
 		txtCorreo = new JTextField( );
@@ -117,11 +109,7 @@ public class DialogoRegistrarEmpresa extends JDialog implements ActionListener{
         
         add( lbNit );
         add( txtNit );
-<<<<<<< HEAD
         
-=======
-
->>>>>>> d36ad856604d5bf05b4ade53b7bafb81df620c6f
         lbDireccion = new JLabel( "Dirección:" );
         txtDireccion = new JTextField( );
         
--- superAndes-jdo/src/main/java/uniandes/isis2304/superAndes/interfazApp/DialogoRegistrarPromocion.java
+++ superAndes-jdo/src/main/java/uniandes/isis2304/superAndes/interfazApp/DialogoRegistrarPromocion.java
@@ -1,5 +1,129 @@
 package uniandes.isis2304.superAndes.interfazApp;
 
-public class DialogoRegistrarPromocion {
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class DialogoRegistrarPromocion extends JDialog implements ActionListener{
+	 /**
+     * Comando para agregar un producto
+     */
+    private final static String REGISTRAR = "REGISTRAR";
+
+    /**
+     * Comando para cancelar la operación
+     */
+    private final static String CANCELAR = "CANCELAR";
+    /**
+     * 
+     */
+	private InterfazSuperAndesApp principal;
+	/**
+     * Etiqueta nombre
+     */
+    private JLabel lbNombre;
+
+    /**
+     * Etiqueta direccion
+     */
+    private JLabel lbCorreo; 
+    
+	/**
+	 * Etiqueta direccion
+	 */
+	private JLabel lbDocumento; 
+
+    /**
+     * Campo de texto para mostrar el nombre
+     */
+    private JTextField txtNombre;
+
+    /**
+     * Campo de texto para mostrar la direccion
+     */
+    private JTextField txtCorreo;
+    
+	/**
+	 * Campo de texto para mostrar la direccion
+	 */
+	private JTextField txtDocumento;
+
+
+    /**
+     * Botón registrar
+     */
+    private JButton btnRegistrar;
+
+    /**
+     * Botón cancelar
+     */
+    private JButton btnCancelar;
+
+    
+
+    // --------------------------------------------------------
+    // Constructores
+    // --------------------------------------------------------
+
+    /**
+     * Construye un nuevo dialogo para agregar un crédito
+     * @param principalP es la referencia al panel padre del diálogo
+     */
+    public DialogoRegistrarPromocion( InterfazSuperAndesApp pPrincipal )
+    {
+        principal = pPrincipal;
+
+        setLayout( new GridLayout( 7, 2 ) );
+        this.setSize( 600, 200 );
+        setTitle( "Agregar proveedor" );
+
+        lbNombre = new JLabel( "Nombre:" );
+        txtNombre = new JTextField( );
+
+        add( lbNombre );
+        add( txtNombre );
+
+        lbCorreo = new JLabel( "Correo:" );
+        txtCorreo = new JTextField( );
+        
+        add( lbCorreo );
+        add( txtCorreo );
+        
+		lbDocumento = new JLabel( "Documento identificación:" );
+		txtDocumento = new JTextField( );
+
+		add( lbDocumento );
+		add( txtDocumento );
+        
+        btnRegistrar = new JButton( "Registrar" );
+        btnRegistrar.setActionCommand( REGISTRAR );
+        btnRegistrar.addActionListener( this );
+
+        btnCancelar = new JButton( "Cancelar" );
+        btnCancelar.setActionCommand( CANCELAR );
+        btnCancelar.addActionListener( this );
+        
+        add( btnRegistrar );
+        add( btnCancelar );
+
+        setModal( true );
+        setLocationRelativeTo( null );
+    }
+    
+	public void actionPerformed(ActionEvent e) {
+		if( e.getActionCommand( ).equals( REGISTRAR ) )
+		{
+			principal.adicionarPersona2(txtDocumento.getText(), txtCorreo.getText(), txtNombre.getText());
+			this.dispose();
+		}
+		else
+		{
+			this.dispose();
+		}
+	}
 }
